650ec5d7cf6adade38fa1987c3f527fe9962429e
marc2k3 <marc2003@live.co.uk>
2025-05-11 14:53:33 +0100

    Nope

--- fb2k_utils/qwr/error_popup.cpp
+++ fb2k_utils/qwr/error_popup.cpp
@@ -1,33 +1,18 @@
 #include <stdafx.h>
-
 #include "error_popup.h"
-
-#include <qwr/delayed_executor.h>
 
 namespace qwr
 {
 
-void ReportErrorWithPopup( const std::string& title, const std::string& errorText )
+void ReportErrorWithPopup(const std::string& title, const std::string& errorText)
 {
-    const auto report = [title, errorText] {
-        assert( core_api::assert_main_thread() );
+    fb2k::inMainThread([title, errorText]
+        {
+            FB2K_console_formatter() << title.c_str() << ":\n" << errorText.c_str() << "\n";
+            popup_message::g_show(errorText.c_str(), title.c_str());
 
-        FB2K_console_formatter() << title.c_str() << ":\n" << errorText.c_str() << "\n";
-        qwr::DelayedExecutor::GetInstance().AddTask( [errorText, title] {
-            popup_message::g_show( errorText.c_str(), title.c_str() );
-        } );
-
-        MessageBeep( MB_ICONASTERISK );
-    };
-
-    if ( core_api::is_main_thread() )
-    {
-        report();
-    }
-    else
-    {
-        ::fb2k::inMainThread2( report );
-    }
+            MessageBeep( MB_ICONASTERISK );
+        });
 }
 
 } // namespace qwr
