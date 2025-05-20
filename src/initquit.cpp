e3503001ed891a84df8ba4b4155f53f2bcd44570
marc2k3 <marc2003@live.co.uk>
2025-05-20 06:58:39 +0100

    Simplify initquit

--- src/initquit.cpp
+++ src/initquit.cpp
@@ -1,16 +1,17 @@
 #include <stdafx.h>
 
+#include <libPPUI/gdiplus_helpers.h>
 #include <config/delayed_package_utils.h>
 #include <events/event_dispatcher.h>
+#include <fb2k/playlist_lock.h>
 #include <js_engine/js_engine.h>
+
 #include <utils/thread_pool_instance.h>
 
 #include <qwr/abort_callback.h>
 #include <qwr/error_popup.h>
 
 #include <Scintilla.h>
-
-#include <unordered_map>
 
 DECLARE_COMPONENT_VERSION(SMP_NAME, SMP_VERSION, SMP_ABOUT);
 VALIDATE_COMPONENT_FILENAME(SMP_DLL_NAME);
@@ -20,135 +21,10 @@ namespace smp::com
     ITypeLibPtr g_typelib;
 }
 
-namespace SubsystemId
-{
-    enum Type : uint32_t
-    {
-        SMP_OLE,
-        SMP_TYPELIB,
-        SMP_SCINTILLA,
-        SMP_GDIPLUS,
-        SMP_WTL,
-        SMP_WTL_AX,
-        SMP_RICHEDIT,
-        ENUM_END
-    };
-}
-
 namespace
 {
-    struct SubsystemError
-    {
-        const char* description = "";
-        uint32_t errorCode = 0;
-    };
-
-    ULONG_PTR g_pGdiToken = 0;
-    CAppModule g_wtlModule;
-    HMODULE g_hRichEdit = nullptr;
-
-    std::unordered_map<SubsystemId::Type, SubsystemError> g_subsystemFailures;
-
-    void InitializeSubsystems()
-    {
-        const auto ins = core_api::get_my_instance();
-
-        if (auto hr = OleInitialize(nullptr);
-            FAILED(hr))
-        {
-            g_subsystemFailures[SubsystemId::SMP_OLE] = { "OleInitialize failed", static_cast<uint32_t>(hr) };
-        }
-
-        {
-            std::array<wchar_t, MAX_PATH> path{};
-            (void)GetModuleFileName(ins, path.data(), path.size()); // NULL-terminated in OS newer than WinXP
-
-            if (auto hr = LoadTypeLibEx(path.data(), REGKIND_NONE, &smp::com::g_typelib);
-                FAILED(hr))
-            {
-                g_subsystemFailures[SubsystemId::SMP_TYPELIB] = { "LoadTypeLibEx failed", static_cast<uint32_t>(hr) };
-            }
-        }
-
-        if (int iRet = Scintilla_RegisterClasses(ins);
-            !iRet)
-        {
-            g_subsystemFailures[SubsystemId::SMP_SCINTILLA] = { "Scintilla_RegisterClasses failed", static_cast<uint32_t>(iRet) };
-        }
-
-        {
-            Gdiplus::GdiplusStartupInput gdip_input;
-            if (auto gdiRet = Gdiplus::GdiplusStartup(&g_pGdiToken, &gdip_input, nullptr);
-                Gdiplus::Ok != gdiRet)
-            {
-                g_subsystemFailures[SubsystemId::SMP_GDIPLUS] = { "OleInitialize failed", static_cast<uint32_t>(gdiRet) };
-            }
-        }
-
-        if (auto hr = g_wtlModule.Init(nullptr, ins);
-            FAILED(hr))
-        {
-            g_subsystemFailures[SubsystemId::SMP_WTL] = { "WTL module Init failed", static_cast<uint32_t>(hr) };
-        }
-
-        // WTL ActiveX support
-        if (!AtlAxWinInit())
-        {
-            g_subsystemFailures[SubsystemId::SMP_WTL_AX] = { "AtlAxWinInit failed", static_cast<uint32_t>(GetLastError()) };
-        }
-
-        {
-            g_hRichEdit = LoadLibraryW(CRichEditCtrl::GetLibraryName());
-            if (!g_hRichEdit)
-            {
-                g_subsystemFailures[SubsystemId::SMP_RICHEDIT] = { "Failed to load RichEdit library", static_cast<uint32_t>(GetLastError()) };
-            }
-        }
-    }
-
-    void FinalizeSubsystems()
-    {
-        if (!g_subsystemFailures.contains(SubsystemId::SMP_RICHEDIT))
-        {
-            FreeLibrary(g_hRichEdit);
-        }
-        if (!g_subsystemFailures.contains(SubsystemId::SMP_WTL))
-        {
-            g_wtlModule.Term();
-        }
-        if (!g_subsystemFailures.contains(SubsystemId::SMP_GDIPLUS))
-        {
-            Gdiplus::GdiplusShutdown(g_pGdiToken);
-        }
-        if (!g_subsystemFailures.contains(SubsystemId::SMP_SCINTILLA))
-        {
-            Scintilla_ReleaseResources();
-        }
-        if (!g_subsystemFailures.contains(SubsystemId::SMP_OLE))
-        {
-            OleUninitialize();
-        }
-    }
-
-    void CheckSubsystemStatus()
-    {
-        if (g_subsystemFailures.empty())
-        {
-            return;
-        }
-
-        std::string errorText =
-            "Spider Monkey Panel initialization failed!\r\n"
-            "The component will not function properly!\r\n"
-            "Failures:\r\n\r\n";
-
-        for (const auto& [key, failure] : g_subsystemFailures)
-        {
-            errorText += fmt::format("{}: error code: {:#x}\r\n", failure.description, failure.errorCode);
-        }
-
-        qwr::ReportErrorWithPopup(SMP_UNDERSCORE_NAME, errorText);
-    }
+    GdiplusScope scope;
+    HMODULE rich_edit_ctrl{};
 
     class InitStageCallback : public init_stage_callback
     {
@@ -167,29 +43,37 @@ namespace
             }
             else if (stage == init_stages::before_ui_init)
             {
-                InitializeSubsystems();
+                try
+                {
+                    smp::PlaylistLockManager::Get().InitializeLocks();
+                }
+                catch (const qwr::QwrException& e)
+                {
+                    qwr::ReportErrorWithPopup(SMP_UNDERSCORE_NAME, fmt::format("Failed to initialize playlist locks: {}", e.what()));
+                }
+
+                const auto ins = core_api::get_my_instance();
+                Scintilla_RegisterClasses(ins);
+                rich_edit_ctrl = LoadLibraryW(CRichEditCtrl::GetLibraryName());
+
+                std::array<wchar_t, MAX_PATH> path{};
+                GetModuleFileNameW(ins, path.data(), path.size());
+                std::ignore = LoadTypeLibEx(path.data(), REGKIND_NONE, &smp::com::g_typelib);
             }
         }
     };
 
-    class InitQuitSmp : public initquit
+    void on_quit()
     {
-    public:
-        void on_init() override
-        {
-            CheckSubsystemStatus();
-        }
-
-        void on_quit() override
-        { // Careful when changing invocation order here!
-            mozjs::JsEngine::GetInstance().PrepareForExit();
-            smp::EventDispatcher::Get().NotifyAllAboutExit();
-            qwr::GlobalAbortCallback::GetInstance().Abort();
-            smp::GetThreadPoolInstance().Finalize();
-            FinalizeSubsystems();
-        }
-    };
+        mozjs::JsEngine::GetInstance().PrepareForExit();
+        smp::EventDispatcher::Get().NotifyAllAboutExit();
+        qwr::GlobalAbortCallback::GetInstance().Abort();
+        smp::GetThreadPoolInstance().Finalize();
+        Scintilla_ReleaseResources();
+        FreeLibrary(rich_edit_ctrl);
+        smp::com::g_typelib.Release();
+    }
 
     FB2K_SERVICE_FACTORY(InitStageCallback);
-    FB2K_SERVICE_FACTORY(InitQuitSmp);
+    FB2K_RUN_ON_QUIT(on_quit);
 }
